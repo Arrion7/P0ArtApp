b6b1b05ac0fba8cad4258d6f025812c3ab2c643e
Arrion7 <102093283+Arrion7@users.noreply.github.com>
2022-04-19 03:41:19 -0400

    Yes Still working

--- UI/Program.cs
+++ UI/Program.cs
@@ -5,6 +5,6 @@ using BL;
 string connectionString = File.ReadAllText("./connectionString.txt");
 
 IRepository repo = new DBRepository(connectionString);
-BL.IAsbl bl = new Asbl(repo);
+IAsbl bl = new Asbl(repo);
 
 new ArtHome(bl).Home();
